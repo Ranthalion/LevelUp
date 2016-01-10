e47dd8f893d9e4bc9c4cb5fa7364672be8572d57
Ranthalion <Ranthalion@hotmail.com>
2016-01-10 18:52:20 -0500

    Laravel setup
    Initial Laravel setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LevelUp
 Skill swap social application
+
+Built with Laravel which requires the following
+-PHP 5.6 or higher (installing on windows requires editing the php.ini file)
+-Composer
+-Vagrant
+-VirtualBox (or other VM software)
---
+++ app/Http/routes.php
@@ -0,0 +1,72 @@
+<?php
+
+use App\Task;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    /**
+	* Dashboard
+	*/
+	Route::get('/', function(){
+		$tasks = Task::orderBy('created_at', 'asc')->get();
+
+		return view('tasks', [
+			'tasks' => $tasks
+		]);
+	});
+	
+	/**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+         $validator = Validator::make($request->all(), [
+			'name' => 'required|max:255',
+		]);
+
+		if ($validator->fails()) {
+			return redirect('/')
+				->withInput()
+				->withErrors($validator);
+		}
+
+		// Create The Task...
+		$task = new Task;
+		$task->name = $request->name;
+		$task->save();
+
+		return redirect('/');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{task}', function (Task $task) {
+        $task->delete();
+
+		return redirect('/');
+    });
+	
+});
